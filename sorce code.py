b71bdc4c8e37b625764b1596d556a49d569686ae
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-12 20:00:15 +0530

    Create sorce code.py

---
+++ sorce code.py
@@ -0,0 +1,71 @@
+import pyttsx3
+import speech_recognition as sr
+import wikipedia
+import datetime
+import os
+import webbrowser
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+
+engine.setProperty('voice', voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+    print("Skynet: ", audio)
+  
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if True:
+        speak("Welcome Back sir!")
+        speak("How may I help you sir!")
+
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone()as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"You: {query}\n")
+        
+       
+    except Exception as e:
+        print(e)
+        print("Sorry I didnt get that , Please say that again")
+        speak("Sorry I didnt get that , Please say that again")
+        return "None"
+    return query
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takecommand().lower()
+
+        if 'wikipedia' in query:
+            speak("Searching wikipedia")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("https:\\www.youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("https:\\www.google.com")
+
+        elif 'play music' in query:
+            music_dir ='C:\\Users\Sagar Solanki\Music'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+            
+        elif 'open code ' in query:
+            codePath = 'A:\\vs code\\Microsoft vs code\\Code.exe'
+            os.startfile(codePath)
